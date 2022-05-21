18b620acfeb9309d6bcba205b8d9c7ae045a5799
josemlozano <josemlozano6@gmail.com>
2022-05-21 18:00:27 +0200

    initial commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Jose Muñoz on 21/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contacts/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Contacts
+//
+//  Created by Jose Muñoz on 21/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+    
+}
